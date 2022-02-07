bfb22e38dca772af5592fed7bbe6c5490cc81bcc
Felipa Mendez <felipalouise@gmail.com>
2022-02-07 15:52:38 -0600

    commit tree b50ae97d96d30f449f3fd3df3dea11c83fa79b20

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+    const intervalID = setTimeout(() => {
+      if (timeRemaining > 0) {
+      setTimeRemaining(timeRemaining-1)}
+      else if(timeRemaining === 0 ) {
+        setTimeRemaining(10)
+        onAnswered(false)
+      }
+    }, 1000)
+    return function () {
+      clearTimeout(intervalID)
+    }
+  }, [timeRemaining])
+
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
